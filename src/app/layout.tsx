624054b38498bbf4dee73da66f2ddd92c292ee1c
Temirlan <ltemirlan2003@gmail.com>
2024-12-26 19:34:29 +0600

    add: app tab icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "JSONplaceholder posts app",
   description: "SONplaceholder posts app - demo",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
 };
 
 export default function RootLayout({
